a58f4731cd88a4c2828a8e9549b13a3a641e5580
Akbari06 <akbarishtiaque@gmail.com>
2025-12-14 00:56:44 +0000

    basic code done, can give runtime of executing code

--- latency.c
+++ latency.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
-#include <string.h>
 
-struct timespec
-{
-    time_t tv_sec; // seconds
-    long tv_nsec; // nanoseconds
-};
-
-int main(void){
+int main(void) {
     struct timespec ts;
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    clock_gettime(CLOCK_MONOTONIC, &ts);
     printf("Seconds: %ld, Nanoseconds: %ld\n", ts.tv_sec, ts.tv_nsec);
+
+    return 0;
 }
